6127c899fd675febed862daecbb875408d036455
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-02-14 15:32:45 +0900

    feat: Initialize Spring Boot project structure

---
+++ src/main/java/com/survey/survey_platform/SurveyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.survey.survey_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/survey/survey_platform/SurveyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.survey.survey_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
