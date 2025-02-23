22e02d104d6484e8c11a27a991194ed0ab7d5f19
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-02-23 11:41:08 +0900

    feat: JPA Auditing 기능 활성화

--- src/main/java/com/survey/survey_platform/SurveyPlatformApplication.java
+++ src/main/java/com/survey/survey_platform/SurveyPlatformApplication.java
@@ -2,8 +2,10 @@ package com.survey.survey_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 생성일시(createdAt)와 수정일시(updatedAt) 같은 감사(audit) 필드를 자동으로 관리하기 위함
 public class SurveyPlatformApplication {
 
 	public static void main(String[] args) {
